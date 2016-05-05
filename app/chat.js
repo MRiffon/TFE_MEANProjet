7a90539bced403cd3d11f383d57b6476acbd9583
MRiffon <m.riffon@students.ephec.be>
2016-05-05 10:06:54 +0200

    Avancement vue, controller Chat coté Client + chat cote Server

--- app/chat.js
+++ app/chat.js
@@ -3,10 +3,32 @@
  */
 
 module.exports = function(io){
+    var users = [];
+
     io.on('connection', function(socket){
+        var username = '';
         console.log('A user has connected');
+
+        socket.on('requestUser', function(){
+            socket.emit('users', {users: users});
+        });
+
+        socket.on('message', function(data){
+            io.emit('message', {username: username, message: data.message});
+        });
+
+        socket.on('addUser', function(data){
+            if(users.indexOf(data.username) == -1){
+                io.emit('addUser', {username: data.username});
+                username = data.username;
+                users.push(data.username);
+            }
+        });
+
         socket.on('disconnect', function(){
-            console.log('A user has disconnected');
+            console.log(username + ' has disconnected');
+            users.splice(users.indexOf(username), 1);
+            io.emit('removeUser', {username: username});
         });
     });
 };
--- public/js/controllers/chatCtrl.js
+++ public/js/controllers/chatCtrl.js
@@ -2,9 +2,44 @@
  * Created by Michaël on 20-04-16.
  */
 
-angular.module('chatCtrl', []).controller('chatController', function($scope, Socket){
+angular.module('chatCtrl', []).controller('chatController', function($scope, Socket, dataFetch){
     Socket.connect();
+    
+    $scope.users = [];
+    $scope.messages = [];
 
+    dataFetch.getProfil().then(function(response){
+        var username = response.data.username;
+    });
+
+    $scope.sendMessage = function(msg){
+        if(msg !== null && msg !== ''){
+            Socket.emit('message', {message: msg});
+        }
+        $scope.msg = '';
+    };
+    
+    Socket.emit('requestUsers', {});
+
+    Socket.emit('addUser', {username: username});
+
+    Socket.on('users', function(data){
+        $scope.users = data.users;
+    });
+
+    Socket.on('message', function(data){
+        $scope.messages.push(data);
+    });
+
+    Socket.on('addUser', function(data){
+        $scope.users.push(data.username);
+    });
+
+    Socket.on('removeUser', function(data){
+        $scope.users.splice($scope.users.indexOf(data.username), 1);
+        $scope.messages.push({username: data.username});
+    });
+    
     $scope.$on('$locationChangeStart', function(event){
         Socket.disconnect(true);
     });
