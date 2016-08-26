b33866e26f6ac92d86bc79ca05e5bbef12c8b21c
Martouf <martinschreinemachers@gmail.com>
2016-08-26 12:09:56 +0200

    Modification port d'écoute du serveur

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ var options = {
 };
 
 // Initialisation serveur http avec socket.io
-var server = require('https').createServer(app, options);
+var server = require('https').createServer(options, app);
 var io = require('socket.io')(server);
 
 
